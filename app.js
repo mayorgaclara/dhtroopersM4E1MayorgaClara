9894c10ca740a588bb3bee732e0aefbc8de7b60b
Clara Mayorga <mayorgaclara@gmail.com>
2021-03-28 01:51:10 +0100

    Agregué formulario producto

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/form.html'));
+});
+
 app.listen(port, () => {
     console.log('Example app listening on port '+ port);
 });
